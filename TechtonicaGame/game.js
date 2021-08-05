c68788c8f76223f5dc0e4ac249a68c9d594f0e06
Shaylan7 <shaylan.hawthorne@gmail.com>
2021-08-05 17:30:15 -0400

    Add counter and previous guesses

--- TechtonicaGame/game.js
+++ TechtonicaGame/game.js
@@ -1,20 +1,45 @@
-import React from 'https://cdn.skypack.dev/react@^17';
-import ReactDOM from 'https://cdn.skypack.dev/react-dom@^17';
+// import React from 'https://cdn.skypack.dev/react@^17';
+// import ReactDOM from 'https://cdn.skypack.dev/react-dom@^17';
 
 
-let chosenNumber = Math.random()*30;
+let chosenNumber = ((Math.random()*100) +1);
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
 
-document.getElementById("submitguess").onclick = function(){
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+const guesscount = document.querySelector('.guesscount');
+
+let guessCount = 1;
+
+// document.getElementById("submitguess").onclick = function(){
+function checkGuess() {
     let guess = document.getElementById("guessField").value;
+
+    if (guessCount === 1) {
+        guesses.textContent = 'Previous guesses: ';
+      }
+      guesses.textContent += guess + ' ';
+      
     if (chosenNumber === guess){
-        alert("Congrats, you won!");
-    } else if (chosenNumber > guess && (chosenNumber - guess >= 5)){
-        alert("Way higher");
-    } else if (chosenNumber > guess && (chosenNumber - guess <= 5)){
-        alert("You're close, a little higher");
-    } else if (chosenNumber < guess && (guess - chosenNumber >= 5)){
-        alert("Way lower");
-    } else if (chosenNumber < guess && (guess - chosenNumber <= 5)){
-        alert("You're close, a little lower");
+        lastResult.textContent = 'Congratulations! You got it right!';
     } 
+    if (guessCount === 10) {
+        lastResult.textContent = "Game Over"
+        } else if (chosenNumber > guess && (chosenNumber - guess >= 10)){
+        lowOrHi.textContent ="Way higher";
+        } else if (chosenNumber > guess && (chosenNumber - guess <= 10)){
+        lowOrHi.textContent = "You're close, a little higher";
+        } else if (chosenNumber < guess && (guess - chosenNumber >= 10)){
+        lowOrHi.textContent ="Way lower"; 
+        } else if (chosenNumber < guess && (guess - chosenNumber <= 10)){
+        lowOrHi.textContent ="You're close, a little lower";
+    } 
+    guessCount++;
+    guesscount.textContent = 'Count of guesses: '+ guessCount;
+    guessField.value = '';
+
 }
+
+guessSubmit.addEventListener('click', checkGuess);
