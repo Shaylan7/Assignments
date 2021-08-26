f0507892eab6a7c0e48d6e456215145cac18cd79
Shaylan Hawthorne <shaylan.hawthorne@gmail.com>
2021-08-25 20:11:07 -0700

    changed fetch call to inside function

--- weather_app/src/Components/Forecast/Forecast.js
+++ weather_app/src/Components/Forecast/Forecast.js
@@ -1,25 +1,51 @@
 import React, { useState } from 'react';
-
-
+import Conditions from '../Conditions/Conditions';
+import classes from './Forecast.module.css';
 
 const Forecast = () => {
     let [responseObj, setResponseObj] = useState({});
-   function getForecast() {
+    let [city, setCity] = useState('');
+    const uriEncodedCity = encodeURIComponent(city);
+    
+
+    //preventDefault is a method called on a javascript event object to prevent default behavior. I.E. default behvior of 
+    //form submit is to refresh, but we use this when we do not want to refresh the page. 
+   
+    function getForecast(e) {
+        e.preventDefault();
+    
+
       // weather data fetch function will go here
-      fetch('https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=fdd1ae281173cb89f06bcc253807103b')
+    
+      fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${uriEncodedCity}&cnt=5&appid=a24f6a95c88938366c0ee6c8e297d505
+      `)
         .then(response => response.json())
         .then(response => {
            setResponseObj(response)
        })
-      
-   }
+    }           
+     
+       
+
+        
    return (
     <div>
     <h2>Find Current Weather Conditions</h2>
     <div>
-        {JSON.stringify(responseObj)}
-    </div>
-    <button onClick={getForecast}>Get Forecast</button>
+        <form onSubmit={getForecast}>
+                <input
+                    type="text"
+                    placeholder="Enter City"
+                    maxLength="50"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    className={classes.textInput}/>
+                <button className={classes.Button}type="submit">Get Forecast</button>
+            </form>
+        <Conditions
+               responseObj={responseObj}
+               />
+    </div> 
 </div>
    )
 }
