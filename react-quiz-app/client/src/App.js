f54f65d835a29bf4f4b7afa602827414a2691a6b
Shaylan Hawthorne <shaylan.hawthorne@gmail.com>
2021-09-01 12:34:24 -0700

    this is rough

---
+++ react-quiz-app/client/src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Questions from "./components/questions.js";
+
+
+function App() {
+  
+
+
+
+  return (
+    <Questions /> 
+  );
+}
+
+export default App;
---
+++ react-quiz-app/client/src/components/questions.js
@@ -0,0 +1,89 @@
+import React, {useState} from "react";
+import totalQuestions from "./data.js";
+import "./questions.css"
+
+export default function Questions(){
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const[color, setButtonColor] = useState('grey'); 
+    const[count, setCount] = useState(0);
+    const [data,setData]=React.useState(null);
+
+
+
+    React.useEffect =() => {
+        fetch('/game')
+          .then((res) => res.json())
+          .then((data) => {
+              console.log(data)
+          setData(data)});
+
+    }
+   
+
+    // const getData=()=>{
+    //     fetch('/game')
+    //       .then(function(response){
+    //         console.log(response)
+    //         return response.json();
+    //       })
+    //       .then(function(myJson) {
+    //         console.log(myJson);
+    //         setData(myJson)
+    //       });
+    //   }
+      
+      
+    // useEffect(()=>{
+    //     getData()
+    //   },[])
+
+//want// 
+    // const handleAnswerButtonClick = (choice) => {
+    //     
+    //     if (nextQuestion < totalQuestions.length){
+    //         setCurrentQuestion(nextQuestion)
+    //     } else {
+    //         alert('you reached the end of the quiz');
+    // }
+    // }
+
+    
+    const colorArr = ['blue', 'pink', 'orange', 'brown', 'yellow', 'green']
+    const handleAnswerButtonClick = (choice) => {
+        const nextQuestion = currentQuestion + 1;
+        const nextCount = count +1; 
+        var randomNum = Math.floor(Math.random()*6) 
+         if (nextQuestion < totalQuestions.length) {
+            setCurrentQuestion(nextQuestion);
+            setButtonColor(colorArr[randomNum]); 
+            console.log(color)
+            setCount(nextCount)
+        } else {
+             alert('you reached the end of the quiz');
+        }
+    
+
+    }
+   
+    return (
+        <div>
+        <div className="content">
+        <div className='question-text'>{totalQuestions[currentQuestion].title}</div>
+        <div className='answer-section'>
+	    {totalQuestions[currentQuestion].choices.map((choice, index) => (
+		<button style={{background:color}} onClick={() =>  handleAnswerButtonClick()}>{choice}</button>
+        ))}
+       </div>
+       </div>
+        <div>
+        {
+            data
+    //    data && data.length>0 && data.map((item)=><p>{item.title}</p>)
+        }
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+        </div>
+    )
+
+      
+    }
