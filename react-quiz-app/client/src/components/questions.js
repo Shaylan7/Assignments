ec99e22a185c0bc47ca8c589225e62de5e9a6827
Shaylan Hawthorne <shaylan.hawthorne@gmail.com>
2021-09-01 20:51:45 -0700

    Map through data - not working

--- react-quiz-app/client/src/components/questions.js
+++ react-quiz-app/client/src/components/questions.js
@@ -6,19 +6,27 @@ export default function Questions(){
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const[color, setButtonColor] = useState('grey'); 
     const[count, setCount] = useState(0);
-    const [data,setData]=React.useState(null);
+    const [data,setData]=React.useState();
 
+   
+  
 
-
-    React.useEffect =() => {
+    React.useEffect(() => {
         fetch('/game')
           .then((res) => res.json())
           .then((data) => {
               console.log(data)
           setData(data)});
+    }, [])
 
-    }
-   
+    // let formattedData = JSON.stringify(data, null, 2)
+
+    // {data.forEach((element, index) => {
+    //     console.log(element.title); 
+    //     console.log(index); 
+    //     })}
+  
+   // why is this second parameter needed ^^^ 
 
     // const getData=()=>{
     //     fetch('/game')
@@ -48,39 +56,46 @@ export default function Questions(){
     // }
 
     
-    const colorArr = ['blue', 'pink', 'orange', 'brown', 'yellow', 'green']
-    const handleAnswerButtonClick = (choice) => {
-        const nextQuestion = currentQuestion + 1;
-        const nextCount = count +1; 
-        var randomNum = Math.floor(Math.random()*6) 
-         if (nextQuestion < totalQuestions.length) {
-            setCurrentQuestion(nextQuestion);
-            setButtonColor(colorArr[randomNum]); 
-            console.log(color)
-            setCount(nextCount)
-        } else {
-             alert('you reached the end of the quiz');
-        }
+    // const colorArr = ['blue', 'pink', 'orange', 'brown', 'yellow', 'green']
+    // const handleAnswerButtonClick = (choice) => {
+    //     const nextQuestion = currentQuestion + 1;
+    //     const nextCount = count +1; 
+    //     var randomNum = Math.floor(Math.random()*6) 
+    //      if (nextQuestion < totalQuestions.length) {
+    //         setCurrentQuestion(nextQuestion);
+    //         setButtonColor(colorArr[randomNum]); 
+    //         console.log(color)
+    //         setCount(nextCount)
+    //     } else {
+    //          alert('you reached the end of the quiz');
+    //     }
     
 
-    }
+    // }
    
     return (
         <div>
         <div className="content">
         <div className='question-text'>{totalQuestions[currentQuestion].title}</div>
+        {console.log('random', data)}
+        <div>
+        <ul>
+            {data.map((value, index) => {
+            return <li key={index}>{value.title}</li>
+            })}
+        </ul>
+        </div>
         <div className='answer-section'>
-	    {totalQuestions[currentQuestion].choices.map((choice, index) => (
+	    {/* {totalQuestions[currentQuestion].choices.map((choice, index) => (
 		<button style={{background:color}} onClick={() =>  handleAnswerButtonClick()}>{choice}</button>
-        ))}
+        ))} */}
        </div>
        </div>
         <div>
-        {
-            data
-    //    data && data.length>0 && data.map((item)=><p>{item.title}</p>)
-        }
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        
+       {/* {data && data.length>0 && data.map((item)=><p>{item.title}</p>)} */}
+        
+        {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
         </div>
         </div>
     )
--- react-quiz-app/server/routes/questions.js
+++ react-quiz-app/server/routes/questions.js
@@ -1,18 +1,21 @@
+const data = require('../data/data.js')
 const userRoutes = (app, fs) => {
     // variables
-    const dataPath = './data/data.json';
+    // const dataPath = './data/data.json';
   
     // READ
+    //load file once and have it in memory 
     app.get('/game', (req, res) => {
-      fs.readFile(dataPath, 'utf8', (err, data) => {
-        if (err) {
-          throw err;
-        }
+            console.log('hello')
+           res.send(data) 
+    //   fs.readFile(dataPath, 'utf8', (err, data) => {
+    //     if (err) {
+    //       throw err;
+    //     }
   
-        res.send(JSON.parse(data));
+    //     res.send(JSON.parse(data));
       });
-    });
-  };
+    };
   
   module.exports = userRoutes;
   
