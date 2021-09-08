5c190be608b5e6bee4e96b23132f39ab57a75b3c
Shaylan Hawthorne <shaylan.hawthorne@gmail.com>
2021-09-07 19:42:14 -0700

    delete event and create search component

--- Eventonica/eventonica-react/src/components/Events.jsx
+++ Eventonica/eventonica-react/src/components/Events.jsx
@@ -1,93 +1,119 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import Search from './Search.jsx'
 
-export default function Events(){
+export default function Events() {
 
 
-    const tame = { name: "Tame Impala", date: '09-10-2021', size: 500, price: 80, id:"1" };
-    const bumbershoot = { name: "Bumbershoot", date: '09-17-2021', size: 800, price: 100, id: "2" };
-    const fair = { name: "Washington State Fair", date: '09-20-2021', size: 400, price: 40, id: "3"};
+    const tame = { eventName: "Tame Impala", date: '09-10-2021', category: 'music', size: 500, price: 80, id: "1" };
+    const bumbershoot = { eventName: "Bumbershoot", date: '09-17-2021', category: 'music', size: 800, price: 100, id: "2" };
+    const fair = { eventName: "Washington State Fair", date: '09-20-2021', category: 'entertainment', size: 400, price: 40, id: "3" };
 
     const [events, setEvents] = useState([tame, bumbershoot, fair]);
+    const [eventName, setEventName] = useState('');
+    const [price, setPrice] = useState('');
+    const [id, setId] = useState('');
+    const [date, setDate] = useState('');
+    const [category, setCategory] = useState('')
+    const [deleteId, setdeleteId] = useState('');
 
 
 
+    //clears form 
+    const clearForm = () => {
+        setEventName('');
+        setId('');
+        setPrice('');
+        setDate('');
+        setCategory('')
+    }
 
+    // adds an event 
     const onSubmit = e => {
         e.preventDefault();
-        const newUser = {id: id, name: name, email: email};
-        setUsers([...users, newUser]);
+        const newEvent = { id: id, eventName: eventName, price: price, date: date, category: category };
+        setEvents([...events, newEvent]);
         clearForm()
-      };
+    };
 
-return (
-<div>
-<section className="event-management">
-          <h2>Event Management</h2>
-          <div>
-            <h3>All Events</h3>
-            <ul id="events-list">
-              {/* Display all Events here */}
-              {events.map((element, i) => { return (<li key={element.id}>Name: {element.name} 
-              Price: ${element.price} Date: {element.date}</li>)})}
-            </ul>
-  
-            <h3>Add Event</h3>
-            <form id="add-event" action="#" onSubmit={onSubmit}>
-              <fieldset>
-                <label>Name</label>
-                <input
-                  type="text"
-                  id="add-event-name"
-                  placeholder="Virtual corgi meetup"
-                value={name} onChange={(e) => setName(e.target.value)}/>
 
-                <label>Price</label> 
-                <input type="text" 
-                id="add-event-price" 
-                placeholder="$0" 
-                value={price} onChange={(e) => setPrice(e.target.value)}/>
+    // deletes an event     
+    const deleteEvent = (deleteId) => {
+        const newEvents = events.filter(i => i.id !== deleteId)
+        setEvents(newEvents)
+    };
 
-                <label>Date</label> 
-                <input type="text" 
-                id="add-event-date" 
-                placeholder="01-01-1999" 
-                value={date} onChange={(e) => setDate(e.target.value)}/>
-         
-              </fieldset>
-              {/* Add more form fields here */}
-              <input type="submit" />
-            </form>
-          </div>
-        </section>
-  
-      <div>
-        <h3>Delete Event</h3>
-        <form id="delete-event" action="#">
-          <fieldset>
-            <label>Event ID</label>
-            <input type="number" min="1" id="delete-event-id" />
-          </fieldset>
-          <input type="submit" />
-        </form>
-      </div>
-  
-      <aside className="search-toolbar">
+    const handleDeleteButtonClick = e => {
+        e.preventDefault();
+        deleteEvent(deleteId)
+
+    }
+
+    return (
         <div>
-          <h3>Find Events</h3>
-          <form id="search" action="#">
-            <fieldset>
-              <label htmlFor="date-search">Date</label>
-              <input type="text" id="date-search" placeholder="YYYY-MM-DD" />
-            </fieldset>
-            <fieldset>
-              <label htmlFor="category-search">Category</label>
-              <input type="text" id="category-search" />
-            </fieldset>
-  
-            <input type="submit" value="Search" />
-          </form>
+            <section className="event-management">
+                <h2>Event Management</h2>
+                <div>
+                    <h3>All Events</h3>
+                    <ul id="events-list">
+                        {/* Display all Events here */}
+                        {events.map((element, i) => {
+                            return (<li key={element.id}>Name: {element.eventName}
+                                Price: ${element.price} Date: {element.date}</li>)
+                        })}
+                    </ul>
+
+                    <h3>Add Event</h3>
+                    <form id="add-event" action="#" onSubmit={onSubmit}>
+                        <fieldset>
+                            <label>Name</label>
+                            <input
+                                type="text"
+                                id="add-event-name"
+                                placeholder="Virtual corgi meetup"
+                                value={eventName} onChange={(e) => setEventName(e.target.value)} />
+
+                            <label>Price</label>
+                            <input type="text"
+                                id="add-event-price"
+                                placeholder="$0"
+                                value={price} onChange={(e) => setPrice(e.target.value)} />
+
+                            <label>Date</label>
+                            <input type="text"
+                                id="add-event-date"
+                                placeholder="01-01-1999"
+                                value={date} onChange={(e) => setDate(e.target.value)} />
+
+                            <label>ID</label>
+                            <input type="text"
+                                id="add-event-date"
+                                value={id} onChange={(e) => setId(e.target.value)} />
+
+                            <label>Category</label>
+                            <input type="text"
+                                id="add-category-date"
+                                value={category} onChange={(e) => setCategory(e.target.value)} />
+
+
+                        </fieldset>
+                        {/* Add more form fields here */}
+                        <input type="submit" />
+                    </form>
+                </div>
+            </section>
+
+            <div>
+                <h3>Delete Event</h3>
+                <form id="delete-event" action="#" onSubmit={handleDeleteButtonClick}>
+                    <fieldset>
+                        <label>Event ID</label>
+                        <input type="number" min="1" id="delete-event-id" value={deleteId} onChange={(e) => setdeleteId(e.target.value)} />
+                    </fieldset>
+                    <input type="submit" />
+                </form>
+            </div>
+            <Search />
+
         </div>
-      </aside>
-      </div> 
-)
-} 
+    )
+}
