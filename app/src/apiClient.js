405fb45aa11785f9309d8bc511971b524e5e8e78
George Song <george@monozuku.com>
2021-09-10 02:57:28 -0700

    feat: user can (un)favorite events
    * Add ability to select "act as" user
    * Filter by favorited events

--- app/src/apiClient.js
+++ app/src/apiClient.js
@@ -1,17 +1,25 @@
+export const getUsers = () => _get("/api/users");
+
+export const getUser = (id) => _get(`/api/users/${id}`);
+
+export const addUser = (user) => _post("/api/users", user);
+
+export const deleteUser = (userId) => _delete(`/api/users/${userId}`);
+
+export const favoriteEvent = (userId, eventId) =>
+  _post(`/api/users/${userId}/favoriteEvent`, { eventId });
+
+export const unfavoriteEvent = (userId, eventId) =>
+  _post(`/api/users/${userId}/unfavoriteEvent`, { eventId });
+
 export const getEvents = async () => {
   const events = await _get("/api/events");
   return events.map((event) => ({ ...event, date: new Date(event.date) }));
 };
 
-export const addEvent = async (event) => _post("/api/events", event);
+export const addEvent = (event) => _post("/api/events", event);
 
-export const deleteEvent = async (eventId) => _delete(`/api/events/${eventId}`);
-
-export const getUsers = async () => _get("/api/users");
-
-export const addUser = async (user) => _post("/api/users", user);
-
-export const deleteUser = async (userId) => _delete(`/api/users/${userId}`);
+export const deleteEvent = (eventId) => _delete(`/api/events/${eventId}`);
 
 const _get = async (url) => (await fetch(url)).json();
 
@@ -21,7 +29,12 @@ const _post = async (url, body) => {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
   });
-  return response.json();
+  let result;
+  try {
+    result = await response.json();
+  } catch {}
+
+  return result;
 };
 
-const _delete = async (url) => fetch(url, { method: "DELETE" });
+const _delete = (url) => fetch(url, { method: "DELETE" });
--- server/users.mjs
+++ server/users.mjs
@@ -8,14 +8,28 @@ userRoutes.get("/", async (request, response) =>
   response.json(await db.getUsers()),
 );
 
-userRoutes.delete("/:userId", async (request, response) => {
-  await db.deleteUser(request.params.userId);
-  response.status(204).end();
-});
+userRoutes
+  .get("/:userId", async (request, response) =>
+    response.json(await db.getUser(request.params.userId)),
+  )
+  .delete("/:userId", async (request, response) => {
+    await db.deleteUser(request.params.userId);
+    response.status(204).end();
+  });
 
 userRoutes.use(express.json());
 userRoutes.post("/", async (request, response) => {
   response.status(201).json(await db.addUser(request.body));
+});
+
+userRoutes.post("/:userId/favoriteEvent", async (request, response) => {
+  await db.favoriteEvent(request.params.userId, request.body.eventId);
+  return response.status(204).end();
+});
+
+userRoutes.post("/:userId/unfavoriteEvent", async (request, response) => {
+  await db.unfavoriteEvent(request.params.userId, request.body.eventId);
+  return response.status(204).end();
 });
 
 export default userRoutes;
