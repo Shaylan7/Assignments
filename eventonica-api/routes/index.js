bb28bfbc948c43da02315224a4ef4932e299d613
Shaylan Hawthorne <shaylan.hawthorne@gmail.com>
2021-09-20 08:57:48 -0700

    last push eventonica

--- eventonica-api/routes/index.js
+++ eventonica-api/routes/index.js
@@ -1,15 +1,3 @@
-var pgp = require('pg-promise')(/* options */)
-var db = pgp('postgres://postgres:Pass2020!@localhost:5432/eventonica')
-
-db.one('SELECT $1 AS value', 123)
-  .then(function (data) {
-    console.log('DATA:', data.value)
-  })
-  .catch(function (error) {
-    console.log('ERROR:', error)
-  })
-
-
 var express = require('express');
 var router = express.Router();
 
--- eventonica-api/routes/users.js
+++ eventonica-api/routes/users.js
@@ -1,12 +1,8 @@
 var express = require('express');
-// Single routing
 var router = express.Router();
-    
 
-  
 
- // using global variable so that it changes when delete function is ran and new user data is sent back  
-var users = [
+let users = [
   { name: "Marlin", email: "marlin@gmail.com", id: "1" },
   { name: "Nemo", email: "nemo@gmail.com", id: "2" },
   { name: "Dory", email: "dory@gmail.com", id: "3" }
@@ -14,44 +10,8 @@ var users = [
 ]
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  console.log('string')
+router.get('/', function(req, res, next) {
   res.send(users);
 });
-
-// Delete user
-router.delete('/:userId', function (req, res) {
-  // save request data to a variable in routes/users.js
-  const userId = req.params.userId
-  console.log('hello',userId)
-  console.log('something')
-  
-  users = users.filter(i => {
-    console.log(i.id)
-    if(i.id != userId){
-      return true; 
-    } else {
-      return false;
-    }
-    
-  })
-  console.log(users)
-
-  res.send('User has been deleted');
-});
-
-// Add user 
-
-router.post('/', function(req, res, next) {
-  // save request data to a variable in routes/users.js
-  const user = req.body;
-
-    // output the book to the console for debugging
-    
-    users.push(user);
-
-  res.send('This user has been added');
-});
-
 
 module.exports = router;
